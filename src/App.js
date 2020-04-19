74d0a8a121042b1e80b27cd980375944ee20fa7b
shobhi1310 <42507632+shobhi1310@users.noreply.github.com>
2020-04-19 21:05:08 +0530

    subtle populator created And position done properly

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header'
+import Populator from './components/Populator'
 
 class App extends React.Component{
   render(){
     return (
       <div id="app">
-        <Header title="Date"/>
+        {/* <Header title="Date"/> */}
+        <Populator/>
       </div>
     )
   }
--- src/components/Populator.js
+++ src/components/Populator.js
@@ -3,9 +3,49 @@ import PropTypes from 'prop-types'
 
 class Populator extends React.Component {
     render(){
+        let date = new Date();
+        let time = date.toTimeString()
         return (
-            <div>
-                
+            <div className="holder">
+                <div className="content">
+                    <div className="node">
+                        <div className="line"></div>
+                        <div className="avatar">
+                            {/* gravatar size now taken 24px */}
+                            <img src="assets/ceo.png" alt="gravatar"/>
+                        </div>
+                    </div>
+                    <div className="context">
+                        <div className="title">id diam vel</div>
+                        <div className="timestamp">{time}</div>
+                    </div>
+                    <div className="comments">
+                        <div className="comment_bubble">
+                            <span className="material-icons">mode_comment</span>
+                        </div>
+                        <div className="count">2</div>
+                    </div>
+                </div>
+                {/* <div>hello</div> */}
+                <div className="content">
+                    <div className="node">
+                        <div className="line"></div>
+                        <div className="avatar">
+                            {/* gravatar size now taken 24px */}
+                            <img src="assets/ceo.png" alt="gravatar"/>
+                        </div>
+                    </div>
+                    <div className="context">
+                        <div className="title">id diam vel</div>
+                        <div className="timestamp">{time}</div>
+                    </div>
+                    <div className="comments">
+                        <div className="comment_bubble">
+                            <span className="material-icons">mode_comment</span>
+                        </div>
+                        <div className="count">2</div>
+                    </div>
+                </div>
             </div>
         )
     }
