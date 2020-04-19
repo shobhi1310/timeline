b73cb6f874f386c2fa16db74712b414892000b10
shobhi1310 <42507632+shobhi1310@users.noreply.github.com>
2020-04-19 14:29:07 +0530

    structure filled somewhat

---
+++ src/components/Populator.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+class Populator extends React.Component {
+    render(){
+        return (
+            <div>
+            </div>
+        )
+    }
+}
+
+export default Populator;
