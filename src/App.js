d5732abcf22b696565d1bbd1d267c0e9b37d8a9c
shobhi1310 <42507632+shobhi1310@users.noreply.github.com>
2020-04-19 14:20:50 +0530

    deleted all shit files to start from scratch

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Header from './Header';
-import Content from './Content';
 
 class App extends React.Component{
   render(){
     return (
-      <div className="notificationsFrame">
-        <div className="panel">
-          <Header title="Timeline"/>
-          {/* <Content /> */}
-        </div>
+      <div id="app">
       </div>
     )
   }
