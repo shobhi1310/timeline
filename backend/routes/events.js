b4779a10aaa63952da0e7af04f75b31355c7abd2
shobhi1310 <42507632+shobhi1310@users.noreply.github.com>
2020-04-21 18:54:38 +0530

    routing initiated with minimal efforts

---
+++ backend/routes/events.js
@@ -0,0 +1,4 @@
+const router = require('express').Router();
+let Events = require('../models/events.models');
+
+module.exports = router;
---
+++ backend/routes/users.js
@@ -0,0 +1,4 @@
+const router = require('express').Router();
+let Users = require('../models/users.model');
+
+module.exports = router;
