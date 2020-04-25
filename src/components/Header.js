1c643be692a3f33baa2f47a6163d7ef0071f7482
shobhi1310 <42507632+shobhi1310@users.noreply.github.com>
2020-04-25 20:49:15 +0530

    proper version of jquery added to script

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,10 +10,10 @@ class Header extends React.Component{
         return (
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <a className="navbar-brand" href="#">Icon</a>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#extraNavs" aria-controls="extraNavs" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div className="collapse navbar-collapse" id="extraNavs">
                     <div className="navbar-nav">
                         <a className="nav-item nav-link active" href="#">Home <span className="sr-only">(current)</span></a>
                         <a className="nav-item nav-link" href="#">Profile</a>
