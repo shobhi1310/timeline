ab4fcf2b2618d360ffd7530cca071ab4f7fd78dd
shobhi1310 <42507632+shobhi1310@users.noreply.github.com>
2020-04-27 18:22:30 +0530

    Home has been broken to Main view component and seperated and route link provided

---
+++ src/components/MainView.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
+import {Link} from 'react-router-dom';
+
+import Populator from './Populator';
+import EventAdder from './EventAdder';
+import Signup from './SignUp';
+
+export class MainView extends Component {
+    render() {
+        return (
+            <div>
+                <div className="picker">
+                    <div className="area">
+                        <DatePicker/>
+                    </div>
+                    <div className="calendar-icon">
+                        <span class="material-icons">event</span>
+                    </div>
+                </div>
+                <div id="timeline">
+                    <Populator/>
+                    <Populator/>
+                </div>
+                <Link to="/event-adder" >
+                    <EventAdder/>
+                </Link>
+            </div>
+        )
+    }
+}
+
+export default MainView
