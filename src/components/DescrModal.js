98ee448976f7bc7d56471e7b85b0fe46c623a7d1
shobhi1310 <42507632+shobhi1310@users.noreply.github.com>
2020-05-12 20:13:33 +0530

    tagged photos also visible

--- src/components/DescrModal.js
+++ src/components/DescrModal.js
@@ -5,7 +5,7 @@ export class DescrModal extends Component {
         const {eventParcel} = this.props
         return (
             <div class="modal fade" id="eventModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog" role="document">
+                <div class="modal-dialog modal-dialog-scrollable" role="document">
                     <div class="modal-content">
                     <div class="modal-header">
                         <h5 class="modal-title" id="exampleModalLabel">{eventParcel.title}</h5>
@@ -15,6 +15,22 @@ export class DescrModal extends Component {
                     </div>
                     <div class="modal-body">
                         {eventParcel.descr}
+                        <div className="tagged photos">
+                            {
+                                (eventParcel.photos.length>0) ? (
+                                    <div className="descr-photo">
+                                    <h3 className="display-4">Tagged Photos</h3>
+                                    {
+                                        (eventParcel.photos).map((each)=>{
+                                            return(
+                                                <img src={Buffer(each.image).toString('utf8')} />
+                                            )
+                                        })
+                                    }
+                                    </div>
+                                ) : null
+                            }
+                        </div>
                     </div>
                     </div>
                 </div>
