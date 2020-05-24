d27bf9de8507a0b5fcccb81896e02c1f74bf50c3
shobhi1310 <42507632+shobhi1310@users.noreply.github.com>
2020-05-24 20:29:20 +0530

    icon added to navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,7 +12,9 @@ class Header extends React.Component{
         date = date.toUTCString();
         return (
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <a className="navbar-brand" href="#">Icon</a>
+                <a className="navbar-brand" href="#">
+                    <img src="./assets/logo.jfif" style={{width:"50px"}} />
+                </a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#extraNavs" aria-controls="extraNavs" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
